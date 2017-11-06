02e3b3744846e78ad59a078d7ac1de36acbe2842
raygabakti <rayga.bakti@gmail.com>
2017-11-07 00:22:34 +0700

    add empty doctor component

--- src/app/masterdata/masterdata.module.ts
+++ src/app/masterdata/masterdata.module.ts
@@ -2,12 +2,17 @@ import {SmartadminModule} from '../shared/smartadmin.module';
 import { NgModule } from '@angular/core';
 
 import {routing} from './masterdata.routing';
+import { DoctorComponent } from './doctor/doctor.component';
+import { AddComponent } from './doctor/add/add.component';
+import { EditComponent } from './doctor/edit/edit.component';
+import { HomeComponent } from './doctor/home/home.component';
+import { ShowComponent } from './doctor/show/show.component';
 
 @NgModule({
   imports: [
     SmartadminModule,
     routing,
   ],
-  declarations: []
+  declarations: [DoctorComponent, AddComponent, EditComponent, HomeComponent, ShowComponent]
 })
 export class MasterdataModule { }
