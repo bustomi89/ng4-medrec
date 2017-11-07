dd527fb62225f8d6691aa5ceb8b0e1193357a739
raygabakti <rayga.bakti@gmail.com>
2017-11-07 10:02:55 +0700

    empty master component

--- src/app/masterdata/masterdata.module.ts
+++ src/app/masterdata/masterdata.module.ts
@@ -7,12 +7,14 @@ import { AddComponent } from './doctor/add/add.component';
 import { EditComponent } from './doctor/edit/edit.component';
 import { HomeComponent } from './doctor/home/home.component';
 import { ShowComponent } from './doctor/show/show.component';
+import { PetComponent } from './pet/pet.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   imports: [
     SmartadminModule,
     routing,
   ],
-  declarations: [DoctorComponent, AddComponent, EditComponent, HomeComponent, ShowComponent]
+  declarations: [DoctorComponent, AddComponent, EditComponent, HomeComponent, ShowComponent, PetComponent, CustomerComponent]
 })
 export class MasterdataModule { }
